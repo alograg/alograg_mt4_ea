11d8edb44320c7158892bb440fab11639ad62ab0
Henry <alograg@gmail.com>
2017-09-13 16:31:53 -0500

    Actualiza IDE

--- format-code.cmd
+++ format-code.cmd
@@ -1,4 +1,4 @@
 @echo off
 for /f %%f in ('dir /s/b *.TMP') do del %%f
-for /f %%f in ('dir /s/b *.mq*') do C:\Users\Henry\.vscode\extensions\ms-vscode.cpptools-0.12.3\LLVM\bin\clang-format.exe -style=LLVM -i %%f
+for /f %%f in ('dir /s/b *.mq*') do C:\Users\Henry\.vscode\extensions\ms-vscode.cpptools-0.12.4\LLVM\bin\clang-format.exe -style=LLVM -i %%f
 for /f %%f in ('dir /s/b *.TMP') do del %%f
