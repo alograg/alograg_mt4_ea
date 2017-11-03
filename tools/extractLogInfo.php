66241fd0b802afa335ed5a7117893f025d362f49
Henry <alograg@gmail.com>
2017-11-03 12:01:41 -0600

    Analisis de spreads mas confirguraciones y cosas del market

--- tools/extractLogInfo.php
+++ tools/extractLogInfo.php
@@ -5,31 +5,56 @@ function cmdPrint($value){
 $files = glob('*.log');
 //echo json_encode($files, JSON_PRETTY_PRINT);
 $lineRegex = '/(?P<type>\d)\s+(?P<time>\d+:\d+:\d+\.\d+)\s+(?P<description>.+)(?P<symbol>\w{6}),(?P<frame>\w+)(\s{0,1}\D)*: (?P<details>.*)/';
+$spreadAtributtes = '/Spread trade = (?P<current>\d+\.+\d+).+(?P<limit>\d+\.*\d+)/';
 $logs = [];
-$fp = fopen('concentrado.csv', 'w');
-fputcsv($fp, [
-    "type",
+@unlink('concentrado.csv');
+@unlink('spreads.csv');
+$logFile = fopen('concentrado.csv', 'w');
+fputcsv($logFile, [
+    "week",
+    "day",
     "time",
-    "description",
+    "type",
     "symbol",
     "frame",
+    "description",
     "details",
+]);
+$spreadFile = fopen('spreads.csv', 'w');
+fputcsv($spreadFile, [
+    "week",
     "day",
+    "time",
+    "type",
+    "symbol",
+    "frame",
+    "description",
+    "details",
+    "spread",
+    "limit",
 ]);
 foreach($files as $file){
     $fileContent = file($file);
     foreach($fileContent as $fileLine){
         preg_match($lineRegex, $fileLine, $line);
+        $date = strtotime(basename($file, '.log'));
         $object = [
-            "type"=>trim($line["type"]),
+            "week"=>date('Y\WW', $date),
+            "day"=>date('w', $date),
             "time"=>trim($line["time"]),
-            "description"=>trim($line["description"]),
+            "type"=>trim($line["type"]),
             "symbol"=>trim($line["symbol"]),
             "frame"=>trim($line["frame"]),
+            "description"=>trim($line["description"]),
             "details"=>trim($line["details"]),
-            "day"=>basename($file, '.log'),
         ];
-        fputcsv($fp, $object);
+        fputcsv($logFile, $object);
+        if(preg_match($spreadAtributtes, $fileLine, $line)){
+            $object['spread'] = intval($line['current']);
+            $object['limit'] = $line['limit'];
+            fputcsv($spreadFile, $object);
+        }
     }
 }
-fclose($fp);
+fclose($logFile);
+fclose($spreadFile);
