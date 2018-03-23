97fbc78e6173f18bbb5316a4dc15e0819249ca42
alograg <alograg@gmail.com>
2018-03-23 16:17:46 +0100

    Actualizacion 20180323

--- format-code.cmd
+++ format-code.cmd
@@ -1,4 +1,4 @@
 @echo off
 for /f %%f in ('dir /s/b *.TMP') do del %%f
-for /f %%f in ('dir /s/b *.mq*') do C:\Users\Henry\.vscode\extensions\ms-vscode.cpptools-0.13.1\LLVM\bin\clang-format.exe -style=LLVM -i %%f
+for /f %%f in ('dir /s/b *.mq*') do C:\Users\alogr\.vscode\extensions\ms-vscode.cpptools-0.15.0\LLVM\bin -style=LLVM -i %%f
 for /f %%f in ('dir /s/b *.TMP') do del %%f
